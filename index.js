cad92303ca5a27f0d182be6fdfd25b99edd7a203
Geoff Thompson <thompsgr@avaion.com>
2018-09-05 09:54:20 -0500

    added mocha tests

--- index.js
+++ index.js
@@ -2,28 +2,47 @@
 // counter object - approximates use of associative arrays for counting in php 
 function counter(desc) {
 
+    // must have description
+    if (desc === undefined) {
+        return -1;
+    }
+
     // use javascript object properties to track items
     let obj = {};
 
+    // increment counter for an item
     function increment(item) {
         if (obj.hasOwnProperty(item)) {
             obj[item] += 1;
         } else {
             obj[item] = 1;
         }
+        return obj[item];
     }
 
-    function log() {
-        console.log(`${desc}:`);
-        // sort items alphabetically, then print out
+    // array of counter items in sorted order
+    function items() {
+        // sort items alphabetically
+        let items = [];
         Object.keys(obj)
               .sort()
               .forEach(function(v, i) {
-                  console.log(`${v}: ${obj[v]}`);
+                  items.push({ item: v, count: obj[v]});
               });
+        return items;
+    }
+
+    // print items to console
+    function log() {
+        let s = `${desc}: \n`;
+        items().forEach(function(v, i) {
+            s += `${v.item}: ${v.count} \n`;
+        });
+        return s;
     }
     return {
         increment: increment,
+        items: items,
         log: log
     }
 }
---
+++ test/counter.js
@@ -0,0 +1,63 @@
+var assert = require('assert');
+var utils = require('../index.js');
+var mycounter;
+describe('counter()', function() {
+
+    beforeEach(function() {
+        mycounter = utils.counter('New Item');
+    });
+
+    describe('constructor', function() {
+        it('should return -1 when no text description is provided', function() {
+            assert.equal(utils.counter(), -1);
+        });
+        it('should return object with increment, items and log methods', function() {
+            assert.equal(typeof mycounter, 'object');
+            assert.equal(typeof mycounter.increment, 'function');
+            assert.equal(typeof mycounter.items, 'function');
+            assert.equal(typeof mycounter.log, 'function');
+        });
+    });
+    describe('increment', function() {
+        it('should return 1 for a new item added to increment', function() {
+            assert.equal(mycounter.increment('Test'),1);
+        });
+        it('should return 2 for an item incremented two times', function() {
+            mycounter.increment('Test');
+            assert.equal(mycounter.increment('Test'),2);
+        });
+    });
+    describe('items', function() {
+        it('should return array', function() {
+            assert.equal(Array.isArray(mycounter.items()), true); 
+        });
+        it('should return array with two items', function() {
+            mycounter.increment('A');
+            mycounter.increment('B');
+            assert.equal(mycounter.items().length,2);
+        });
+        it('should return items sorted alphabetically', function() {
+            mycounter.increment('B');
+            mycounter.increment('D');
+            mycounter.increment('A');
+            mycounter.increment('C');
+            assert.equal(mycounter.log(),'New Item: \nA: 1 \nB: 1 \nC: 1 \nD: 1 \n');
+        });
+    });
+    describe('log', function() {
+        it('should return description with no items', function() {
+            assert.equal(mycounter.log(),'New Item: \n');
+        });
+        it('should return description with items', function() {
+            mycounter.increment('A');
+            mycounter.increment('B');
+            mycounter.increment('B');
+            assert.equal(mycounter.log(),'New Item: \nA: 1 \nB: 2 \n');
+        });
+    });
+    describe.skip('skip', function() {
+        it('should return pending', function() {
+            throw new Error("fail");
+        });
+    });
+});
